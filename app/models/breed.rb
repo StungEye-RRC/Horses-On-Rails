ac1358223741c6efec15a2f73412dd869f67d436
Kyle Geske <stungeye@gmail.com>
2019-10-22 08:36:08 -0500

    Validations and associations on the models.

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Breed < ApplicationRecord
+  has_many :horses
+
+  validates :name, presence: true
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,5 @@
 class Horse < ApplicationRecord
   belongs_to :breed
+
+  validates :name, presence: true
 end
