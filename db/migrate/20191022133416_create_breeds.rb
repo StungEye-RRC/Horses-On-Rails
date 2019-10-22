e02354b7ab8537f9191caeb04691e04582f5d7e0
Kyle Geske <stungeye@gmail.com>
2019-10-22 08:43:10 -0500

    Added Faker and seed script.

--- db/migrate/20191022133416_create_breeds.rb
+++ db/migrate/20191022133416_create_breeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateBreeds < ActiveRecord::Migration[6.0]
   def change
     create_table :breeds do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+Horse.destroy_all
+Breed.destroy_all
+
+NUMBER_OF_BREEDS = 5
+HORSES_PER_BREED = 15
+
+NUMBER_OF_BREEDS.times do
+  breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
+
+  HORSES_PER_BREED.times do
+    breed.horses.create(name: Faker::Creature::Horse.unique.name)
+  end
+end
+
+puts "Created #{Breed.count} Breeds."
+puts "Created #{Horse.count} Horses."
