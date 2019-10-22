88ff367abbcf49f0e684f4e4a57d8d1ffe8b12f4
Kyle Geske <stungeye@gmail.com>
2019-10-22 09:22:30 -0500

    Implemented the actions in the breeds and horses controllers.

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 class BreedsController < ApplicationController
+  # GET /
   def index
+    @breeds = Breed.order(:name)
   end
 
+  # GET /breeds/:id
   def show
+    @breed = Breed.find(params[:id])
   end
 end
--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class HorsesController < ApplicationController
-  def show; end
+  # GET /horses/:id
+  def show
+    @horse = Horse.find(params[:id])
+  end
 end
