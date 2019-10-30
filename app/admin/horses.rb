8cc3822b15acbb47bdbf828713dccb38d7555d20
Kyle Geske <stungeye@gmail.com>
2019-10-30 15:02:33 -0500

    Added an attached image for horses using active storage.

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Horse do
-  permit_params :name, :breed_id, :age, :number_of_legs, :top_speed
+  permit_params :name, :breed_id, :age, :number_of_legs, :top_speed, :image
+
+  form do |f| # This is a formtastic form builder.
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class Horse < ApplicationRecord
   belongs_to :breed
 
   validates :name, presence: true
+
+  has_one_attached :image
 end
