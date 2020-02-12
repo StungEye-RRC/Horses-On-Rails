101a7d0b079aeb374747f3052577efd38aa6a38a
Kyle Geske <stungeye@gmail.com>
2020-02-12 11:10:55 -0600

    Added a simple horse partial.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,12 @@ NUMBER_OF_BREEDS.times do
   breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
 
   HORSES_PER_BREED.times do
-    breed.horses.create(name: Faker::Creature::Horse.unique.name)
+    breed.horses.create(
+      name:           Faker::Creature::Horse.unique.name,
+      age:            rand(3..348),
+      number_of_legs: rand(1..27),
+      top_speed:      3.1415926 * rand(20..56)
+    )
   end
 end
 
