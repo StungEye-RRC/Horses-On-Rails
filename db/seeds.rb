9e8b910f326c8b0db1995265b58e39e5ef84f87e
Kyle Geske <stungeye@gmail.com>
2020-03-11 17:10:37 -0500

    Auto add horse images from unsplash.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,22 @@
 Horse.destroy_all
 Breed.destroy_all
 
-NUMBER_OF_BREEDS = 5
-HORSES_PER_BREED = 15
+NUMBER_OF_BREEDS = 2
+HORSES_PER_BREED = 4
 
 NUMBER_OF_BREEDS.times do
   breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
 
   HORSES_PER_BREED.times do
-    breed.horses.create(
+    horse = breed.horses.create(
       name:           Faker::Creature::Horse.unique.name,
       age:            rand(3..348),
       number_of_legs: rand(1..27),
       top_speed:      3.1415926 * rand(20..56)
     )
+    downloaded_image = open(URI.escape("https://source.unsplash.com/600x600/?#{[horse.name, breed.name].join(',')}"))
+    horse.image.attach(io: downloaded_image, filename: "m-#{[horse.name, breed.name].join('-')}.jpg")
+    sleep(1) # Throttle a tad. Needed?
   end
 end
 
