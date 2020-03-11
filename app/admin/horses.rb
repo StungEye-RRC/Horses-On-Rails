d2c0bcf1ce048e810bb5acd2fb460a7b1cbd94d6
Kyle Geske <stungeye@gmail.com>
2020-03-11 16:18:21 -0500

    Added an horse image using active storage and active admin.

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Horse do
-  permit_params :name, :breed_id, :age, :top_speed, :number_of_legs
+  permit_params :name, :breed_id, :age, :top_speed, :number_of_legs, :image
+
+  # Formtastic A Form Builder
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,4 +2,6 @@ class Horse < ApplicationRecord
   belongs_to :breed
 
   validates :name, presence: true
+
+  has_one_attached :image # horse_object.image.present?
 end
